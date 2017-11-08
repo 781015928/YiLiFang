5fddd4e40c8103a856841cf258b4b508d2d9dc4a
czg <781015928@qq.com>
2017-11-08 18:43:15 +0800

    整合mvc

---
+++ E3-Manager/E3-Dao/src/main/java/com/czg/shop/mapper/ItemMapper.java
@@ -0,0 +1,32 @@
+package com.czg.shop.mapper;
+
+import com.czg.shop.pojo.Item;
+import com.czg.shop.pojo.ItemExample;
+
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface ItemMapper {
+    int countByExample(ItemExample example);
+
+    int deleteByExample(ItemExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Item record);
+
+    int insertSelective(Item record);
+
+    List<Item> selectByExample(ItemExample example);
+
+    Item selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Item record, @Param("example") ItemExample example);
+
+    int updateByExample(@Param("record") Item record, @Param("example") ItemExample example);
+
+    int updateByPrimaryKeySelective(Item record);
+
+    int updateByPrimaryKey(Item record);
+}
---
+++ E3-Manager/E3-Dao/src/main/java/com/czg/shop/mapper/OrderItemMapper.java
@@ -0,0 +1,32 @@
+package com.czg.shop.mapper;
+
+import com.czg.shop.pojo.OrderItem;
+import com.czg.shop.pojo.OrderItemExample;
+
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface OrderItemMapper {
+    int countByExample(OrderItemExample example);
+
+    int deleteByExample(OrderItemExample example);
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    List<OrderItem> selectByExample(OrderItemExample example);
+
+    OrderItem selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") OrderItem record, @Param("example") OrderItemExample example);
+
+    int updateByExample(@Param("record") OrderItem record, @Param("example") OrderItemExample example);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
